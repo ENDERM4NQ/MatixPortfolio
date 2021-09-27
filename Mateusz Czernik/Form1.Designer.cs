976abc7c19211e33a6f358a9dfa6b22f53f9e906
ENDERM4NQ <87671633+ENDERM4NQ@users.noreply.github.com>
2021-09-27 19:27:19 +0100

    Add files via upload

---
+++ Mateusz Czernik/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿
+namespace Mateusz_Czernik
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnMinimize = new System.Windows.Forms.Button();
+            this.btnName = new System.Windows.Forms.Button();
+            this.btnSurname = new System.Windows.Forms.Button();
+            this.btnCallme = new System.Windows.Forms.Button();
+            this.btnAdress = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAboutMe = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.Silver;
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnExit.ForeColor = System.Drawing.Color.DarkRed;
+            this.btnExit.Location = new System.Drawing.Point(704, 3);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(31, 23);
+            this.btnExit.TabIndex = 0;
+            this.btnExit.Text = "X";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnMinimize
+            // 
+            this.btnMinimize.BackColor = System.Drawing.Color.Silver;
+            this.btnMinimize.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnMinimize.Location = new System.Drawing.Point(667, 3);
+            this.btnMinimize.Name = "btnMinimize";
+            this.btnMinimize.Size = new System.Drawing.Size(31, 23);
+            this.btnMinimize.TabIndex = 1;
+            this.btnMinimize.Text = "-";
+            this.btnMinimize.UseVisualStyleBackColor = false;
+            this.btnMinimize.Click += new System.EventHandler(this.btnMinimize_Click);
+            // 
+            // btnName
+            // 
+            this.btnName.BackColor = System.Drawing.Color.Silver;
+            this.btnName.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnName.Font = new System.Drawing.Font("Arial", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.btnName.Location = new System.Drawing.Point(32, 68);
+            this.btnName.Name = "btnName";
+            this.btnName.Size = new System.Drawing.Size(86, 26);
+            this.btnName.TabIndex = 2;
+            this.btnName.Text = "Name";
+            this.btnName.UseVisualStyleBackColor = false;
+            this.btnName.Click += new System.EventHandler(this.btnName_Click);
+            // 
+            // btnSurname
+            // 
+            this.btnSurname.BackColor = System.Drawing.Color.Silver;
+            this.btnSurname.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSurname.Font = new System.Drawing.Font("Arial", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.btnSurname.Location = new System.Drawing.Point(32, 100);
+            this.btnSurname.Name = "btnSurname";
+            this.btnSurname.Size = new System.Drawing.Size(86, 26);
+            this.btnSurname.TabIndex = 3;
+            this.btnSurname.Text = "Surname";
+            this.btnSurname.UseVisualStyleBackColor = false;
+            this.btnSurname.Click += new System.EventHandler(this.btnSurname_Click);
+            // 
+            // btnCallme
+            // 
+            this.btnCallme.BackColor = System.Drawing.Color.Silver;
+            this.btnCallme.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCallme.Font = new System.Drawing.Font("Arial", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.btnCallme.Location = new System.Drawing.Point(32, 132);
+            this.btnCallme.Name = "btnCallme";
+            this.btnCallme.Size = new System.Drawing.Size(86, 26);
+            this.btnCallme.TabIndex = 4;
+            this.btnCallme.Text = "Call me";
+            this.btnCallme.UseVisualStyleBackColor = false;
+            this.btnCallme.Click += new System.EventHandler(this.btnCallme_Click);
+            // 
+            // btnAdress
+            // 
+            this.btnAdress.BackColor = System.Drawing.Color.Silver;
+            this.btnAdress.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnAdress.Font = new System.Drawing.Font("Arial", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.btnAdress.Location = new System.Drawing.Point(32, 164);
+            this.btnAdress.Name = "btnAdress";
+            this.btnAdress.Size = new System.Drawing.Size(86, 26);
+            this.btnAdress.TabIndex = 5;
+            this.btnAdress.Text = "Adress";
+            this.btnAdress.UseVisualStyleBackColor = false;
+            this.btnAdress.Click += new System.EventHandler(this.btnAdress_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Silver;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(650, 417);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 29);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Credits";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.btnExit);
+            this.panel1.Controls.Add(this.btnMinimize);
+            this.panel1.Location = new System.Drawing.Point(0, -3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(738, 32);
+            this.panel1.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(0, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 21);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Matix Dox 2.0";
+            // 
+            // btnAboutMe
+            // 
+            this.btnAboutMe.BackColor = System.Drawing.Color.Silver;
+            this.btnAboutMe.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnAboutMe.Font = new System.Drawing.Font("Arial", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.btnAboutMe.Location = new System.Drawing.Point(32, 196);
+            this.btnAboutMe.Name = "btnAboutMe";
+            this.btnAboutMe.Size = new System.Drawing.Size(86, 26);
+            this.btnAboutMe.TabIndex = 9;
+            this.btnAboutMe.Text = "About Me";
+            this.btnAboutMe.UseVisualStyleBackColor = false;
+            this.btnAboutMe.Click += new System.EventHandler(this.btnAboutMe_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-43, 228);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(131, 116);
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(737, 458);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnAboutMe);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnAdress);
+            this.Controls.Add(this.btnCallme);
+            this.Controls.Add(this.btnSurname);
+            this.Controls.Add(this.btnName);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Mateusz Czernik";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnMinimize;
+        private System.Windows.Forms.Button btnName;
+        private System.Windows.Forms.Button btnSurname;
+        private System.Windows.Forms.Button btnCallme;
+        private System.Windows.Forms.Button btnAdress;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAboutMe;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Mateusz Czernik/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mateusz_Czernik
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+
+        private void btnMinimize_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void btnName_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Mateusz");
+        }
+
+        private void btnSurname_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Czernik");
+        }
+
+        private void btnCallme_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Nr1: +48602712983 (do Starego)" + " " + "Nr2: +48664051790 (do Matki)");
+        }
+
+        private void btnAdress_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Chmielna 75, Rzeszow");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Tool Dev: ENDERMANQ");
+        }
+
+        private void btnAboutMe_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("I am a Minecraft Skid developer, Liar, scammer, Powerfull DDoser, Hypocrite");
+        }
+    }
+}
